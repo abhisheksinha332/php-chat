4e5fca2da0c3e9579a0b12edc6c1f678fb148c5d
Abhishek Sinha <3327abhishek@gmail.com>
2021-04-17 22:14:02 +0530

    online db working

--- Script/auth/signup-config.php
+++ Script/auth/signup-config.php
@@ -1,7 +1,7 @@
 <?php
 
-$con = mysqli_connect("localhost","root","","chat-app");
-
+// $con = mysqli_connect("localhost","root","","chat-app");
+$con = mysqli_connect("remotemysql.com","SoL1qfXgsW","CvieyngFhG","SoL1qfXgsW");
 echo $con ?   null : mysqli_connect_error();
 
 ?>
